4437427bbce0c1e0d7e2eea47135db8fec796bd8
lena <lena@example.com>
2026-08-03 19:55:02 +0200

    fix(fonts): add noto-fonts, roboto & liberation_ttf for Flutter apps (LocalSend)

--- core/fonts.nix
+++ core/fonts.nix
@@ -1,8 +1,18 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    iosevka-bin
-    nerd-fonts.symbols-only
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+    packages = with pkgs; [
+      iosevka-bin
+      nerd-fonts.symbols-only
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      liberation_ttf
+      roboto
+      ubuntu_font_family
+    ];
+  };
 }
