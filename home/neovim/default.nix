13de6490469bfe7d505a734459384216d723f19b
lena <lena@example.com>
2026-08-11 11:49:21 +0200

    feat(neovim): fix cursor styling

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -561,6 +561,8 @@ Réponds uniquement avec le texte final.
     initLua = ''
       -- Options de base
       vim.g.mapleader = " "
+      vim.opt.termguicolors = true
+      vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
       vim.opt.tabstop = 2
       vim.opt.softtabstop = 2
       vim.opt.shiftwidth = 2
